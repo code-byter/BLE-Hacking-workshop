3b85b068f98d802857552b302b0fdb4791d08018
Diego Porras <diego.porras@owasp.org>
2023-10-07 17:21:07 +0200

    GATT_2: Running the server with an empty service

--- 0x03_GATT_SERVER/gatt_server_bless.py
+++ 0x03_GATT_SERVER/gatt_server_bless.py
@@ -10,16 +10,17 @@ def my_read_callback(uuid):
     pass
 
 async def main(loop):
-    service_name = "Basic GATT server: hwh_"
+    service_name = "Basic GATT server hwh_"
     server = BlessServer(name=service_name, loop=loop)
     server.read_request = my_read_callback
 
-    custom_service_uuid = ""
-    server.add_new_service(custom_service_uuid)
+    custom_service_uuid = "ca59d6f6-88fd-4902-8b87-5d97e9d81b93"
+    await server.add_new_service(custom_service_uuid)
 
     characteristic_uuid = ""
 
     await server.start()
-
+    await asyncio.sleep(120)
+    
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main(loop))
