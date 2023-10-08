5773af5d7a4f3a649fd15eb44ac85dfc2e704b84
Diego Porras <diego.porras@owasp.org>
2023-10-08 23:32:07 +0200

    CTF_02: Waling the GATT tree

--- 0x04_BLE_CTF/simple_client.py
+++ 0x04_BLE_CTF/simple_client.py
@@ -1,5 +1,10 @@
 import asyncio
-from bleak import *
+from bleak import (
+    BleakScanner,
+    BleakClient,
+    BleakGATTServiceCollection,
+    BleakGATTCharacteristic
+)
 
 address = "30:AE:A4:97:90:26"
 
@@ -11,6 +16,13 @@ async def main(address=address):
     target_device = await BleakScanner.find_device_by_address(address)
     async with BleakClient(target_device) as client:
         gatt_service_collection: BleakGATTServiceCollection =  client.services
-        print(gatt_service_collection.services.keys())
+        for service in gatt_service_collection.services.values():
+            print(service.description)
+            for characteristic in service.characteristics:
+                print(characteristic)
 
+
+
+
+    
 asyncio.run(main())
